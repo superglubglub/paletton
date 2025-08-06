f8728177d98dcd8768ee006052f0477140b5e4e9
superglubglub <maxyslater@pm.me>
2025-08-05 20:19:45 -0700

    initial work commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,37 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
 import './app.css'
+
+interface ColorOptionParams {
+    title: string;
+}
+
+const ColorOption = ({title: title}: ColorOptionParams) => {
+    return(
+        <div class="w-full flex flex-row items-center">
+            <h2 class="w-auto text-xl text-black font-bold px-3 pb-1">{title}</h2>
+            <div class="bg-red-500 w-full rounded-r-lg h-1/2"></div>
+        </div>
+    )
+}
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+      <div className="w-screen h-screen flex flex-row bg-white">
+          <div className="w-1/4 h-screen flex flex-col border border-red-500 bg-slate-100">
+              <div className="h-auto w-full px-6">
+                  <h1 className="text-black text-xl font-black text-left">_palleton</h1>
+                  <h1 className="text-black text-xl font-black text-right">tester</h1>
+              </div>
+              <div className="h-auto w-full px-6 flex flex-col">
+                  <ColorOption title=".classname"/>
+                  <ColorOption title=".bg"/>
+                  <ColorOption title=".primary"/>
+              </div>
+          </div>
+          <div className="w-full h-screen flex flex-col border border-red-500 bg-white">
+          </div>
       </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
   )
 }
